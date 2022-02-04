998048fc30ac0df9de50e46a8c404d19ce9d4778
fazaly <97007764+fazaly@users.noreply.github.com>
2022-02-04 14:07:02 +0600

    first

---
+++ resources/js/script.js
@@ -0,0 +1,35 @@
+// MixitUp
+
+var mixer = mixitup('.grid-portfolio-container');
+
+// Scrollify
+
+$(function() {
+  $.scrollify({
+    section : ".scrollify",
+    setHeights: false,
+    updateHash: false,
+    scrollSpeed: 500,
+  });
+});
+
+// HAMBURGER MENU
+
+function openMenu() {
+  document.getElementById('navbar').style.height="100%"
+}
+
+function closeMenu() {
+  document.getElementById('navbar').style.height="0%"
+}
+
+// SMOOTH SCROLL
+
+$(function(){
+  $('.menu-item a, .scroll-down a').on('click', function(){
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+    return false;
+  });
+});
